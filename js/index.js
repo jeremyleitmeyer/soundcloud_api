b1b617dffbf1f377ad4b488302cc9e5e33c1c860
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-08-27 20:13:03 -0400

    add next function, still bugs

--- js/index.js
+++ js/index.js
@@ -1,45 +1,52 @@
-var playSong = document.getElementById("playSong")
-var pauseSong = document.getElementById("pauseSong")
-var help
+var playSong = document.querySelector("#playSong")
+var pauseSong = document.querySelector("#pauseSong")
+var nextSong = document.querySelector("#nextSong")
+var songs = []
+var j
 
 SC.initialize ({
   client_id: "fd4e76fc67798bfa742089ed619084a6"
 })
 
-SC.get("/tracks").then(function(response) {
+window.onload = SC.get("/tracks", {
+	q:"yuc'e"
+}).then(function(response) {
+	j = 0
   info = document.querySelector("div")
+  while (songs.length < 10) {
+  	for (i = 0; i < response.length; i++) {
+  		songs.push(response[i])
+  	}
+  }
 
 
   template = `
   <div class="container">
     <div class = "image">
-      ${response[0].artwork_url ? ('<img src="{{artwork_url}}" width="150px" height:"150px">') : ""}
+      ${songs[j].artwork_url ? ('<img src="{{artwork_url}}" width="150px" height:"150px">') : ""}
     </div>
     <h2 class="title"><a href="{{permalink_url}}">{{title}}</a></h2><hr>
     <h2 class="artist"><a href="{{user.permalink_url}}">{{user.username}}</a></h2>
-    ${response[0].genre ? ('<p class="genre">Genre: {{genre}}</p>') : ""}
-    ${response[0].release_year ? ('<p class="release">Released in: {{release_year}}</p>') : ""}
-    <p id="id" data-id={{id}}></p>
+    ${songs[j].genre ? ('<p class="genre">Genre: {{genre}}</p>') : ""}
+    ${songs[j].release_year ? ('<p class="release">Released in: {{release_year}}</p>') : ""}
   </div>  
 `
-  info.innerHTML = Mustache.render(template, response[0]);
+  info.innerHTML = Mustache.render(template, songs[j]);
+});
 
-  help = document.getElementById("id")
-  });
+function playerPlay() {
+  SC.stream( '/tracks/' + songs[j].id ).then(function(player){
+      player.play();    
+    });
+    	pauseSong.addEventListener("click", function(){
+	    player.pause();
+	  })
+	  	nextSong.addEventListener("click", function(){
+	    j++
+	    playerPlay()
+	  })    
+  };
 
-  
-playSong.addEventListener("click", function(){
 
-  SC.stream( '/tracks/' + help.dataset.id ).then(function(player){
-
-    pauseSong.addEventListener("click", function(){
-    player.pause();
-  })
-    console.log(player)
-
-    player.play();
-  })
-})
-  
 
 
